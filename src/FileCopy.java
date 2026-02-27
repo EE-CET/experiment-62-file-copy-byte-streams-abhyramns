fe7e218f75c3c8cef4cca6d72cff2dd5c95725b6
abhyramns <abhyramns@gmail.com>
2026-02-27 10:28:24 +0530

    code updated

--- src/FileCopy.java
+++ src/FileCopy.java
@@ -7,25 +7,20 @@ public class FileCopy {
         String sourceFile = "source.txt";
         String destFile = "destination.txt";
 
-        int byteData;
         // Part 1: Copy from source to destination using Byte Streams
         try (FileInputStream fis = new FileInputStream(sourceFile);
              FileOutputStream fos = new FileOutputStream(destFile)) {
-
-            // TODO: Declare an integer variable to hold the byte read
+            
+            // Declare an integer variable to hold the byte read
             int byteData;
             
-
-            // TODO: Read from 'fis' until it returns -1
-            // TODO: Write the byte read to 'fos'
+            // Read from 'fis' until it returns -1
             while ((byteData = fis.read()) != -1) {
-                fos.write(byteData);}
-
+                // Write the byte read to 'fos'
+                fos.write(byteData);
+            }
 
         } catch (IOException e) {
-        } 
-        catch (IOException e) 
-        {
             System.out.println("Error during file copy: " + e.getMessage());
             return; // Exit if copy fails
         }
@@ -34,17 +29,15 @@ public class FileCopy {
 
         // Part 2: Read and display the contents of the newly created destination file
         try (FileInputStream fis = new FileInputStream(destFile)) {
+            
+            int byteData;
 
-            // TODO: Read from 'fis' and print each byte as a character to the console
-            // Hint: use (char) to cast the integer byte to a character before printing
+            // Read from 'fis' and print each byte as a character
             while ((byteData = fis.read()) != -1) {
                 System.out.print((char) byteData);
             }
 
         } catch (IOException e) {
-        } 
-        catch (IOException e) 
-        {
             System.out.println("Error reading destination file: " + e.getMessage());
         }
     }
